3290127bddb969e5a7463d126ed00c7267a054dd
Ying Hang Eng <eyinghang@gmail.com>
2018-01-02 17:40:44 -0500

    android support

--- android/src/main/java/com/robinpowered/RNAudioJack/RNAudioJackModule.java
+++ android/src/main/java/com/robinpowered/RNAudioJack/RNAudioJackModule.java
@@ -2,20 +2,79 @@ package com.robinpowered.RNAudioJack;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableNativeMap;
+
+import java.util.Map;
+import java.util.HashMap;
+import javax.annotation.Nullable;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.media.AudioDeviceInfo;
 
 public class RNAudioJackModule extends ReactContextBaseJavaModule {
+    private static final String MODULE_NAME = "RNAudioJack";
+    private static final String AUDIO_CHANGED_NOTIFICATION = "AUDIO_CHANGED_NOTIFICATION";
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNAudioJackModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNAudioJackModule(final ReactApplicationContext reactContext) {
+        super(reactContext);
 
-  @Override
-  public String getName() {
-    return "RNAudioJack";
-  }
+        IntentFilter headphonesFilter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+        BroadcastReceiver headphonesReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int pluggedInState = intent.getIntExtra("state", -1);
+
+                WritableNativeMap data = new WritableNativeMap();
+                data.putBoolean("audioJackPluggedIn", pluggedInState == 1);
+
+                if (reactContext.hasActiveCatalystInstance()) {
+                    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(AUDIO_CHANGED_NOTIFICATION,
+                        data);
+                }
+            }
+        };
+        reactContext.registerReceiver(headphonesReceiver, headphonesFilter);
+    }
+
+    private boolean isHeadSetPluggedIn() {
+        AudioManager audioManager = (AudioManager) getReactApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return audioManager.isWiredHeadsetOn();
+        } else {
+            AudioDeviceInfo[] devices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
+            for (int i = 0; i < devices.length; i++) {
+                AudioDeviceInfo device = devices[i];
+                if (device.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public @Nullable Map<String, Object> getConstants() {
+        HashMap<String, Object> constants = new HashMap<>();
+        constants.put(AUDIO_CHANGED_NOTIFICATION, AUDIO_CHANGED_NOTIFICATION);
+        return constants;
+    }
+
+    @Override
+    public String getName() {
+        return MODULE_NAME;
+    }
+
+    @ReactMethod
+    public void isAudioJackPluggedIn(final Promise promise) {
+        promise.resolve(isHeadSetPluggedIn());
+    }
 }
